1cbc94215604532da79467882ba67cb3da637ba2
44rereren <44rereren@gmail.com>
2020-06-27 16:52:30 +0900

    add pure_localization.launch

--- src/cartographer_ros/cartographer_ros/configuration_files/pure_localization.lua
+++ src/cartographer_ros/cartographer_ros/configuration_files/pure_localization.lua
@@ -2,8 +2,8 @@ include "backpack_2d.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true;
 
-TRAJECTORY_BUILDER_2D.min_range = 0.
-TRAJECTORY_BUILDER_2D.max_range = 7.
+TRAJECTORY_BUILDER_2D.min_range = 0.01
+TRAJECTORY_BUILDER_2D.max_range = 6.
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 0.
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 2
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-2
@@ -11,7 +11,7 @@ TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(5.)
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
 TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.05 --0.05
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.025
+TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05 --0.025
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1. --7
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 0.01 --15
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = true
